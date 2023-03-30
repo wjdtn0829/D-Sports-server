0d3033504109408e4158aac68ea6142c94df33de
wjdtn0829 <oh050211@gmail.com>
2023-03-30 12:04:40 +0900

    first commit

---
+++ D-Sprots-server/src/test/java/com/example/dsprotsserver/DSprotsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsprotsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSprotsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
