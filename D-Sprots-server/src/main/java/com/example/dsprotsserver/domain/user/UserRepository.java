5574bf5aa4f98d6b03f3ee3cfd71d61b7a4f599d
wjdtn0829 <oh050211@gmail.com>
2023-04-04 19:47:47 +0900

    feat :: add user repository

---
+++ D-Sprots-server/src/main/java/com/example/dsprotsserver/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.dsprotsserver.domain.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
