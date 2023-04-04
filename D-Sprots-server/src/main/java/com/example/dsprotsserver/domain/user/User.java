2c58152b24cce464b758b24b87c4ab7d538062eb
wjdtn0829 <oh050211@gmail.com>
2023-04-04 19:47:20 +0900

    feat :: edit user entity

--- D-Sprots-server/src/main/java/com/example/dsprotsserver/domain/user/User.java
+++ D-Sprots-server/src/main/java/com/example/dsprotsserver/domain/user/User.java
@@ -5,15 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="user")
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @NotNull
     @Column(length = 4)
@@ -28,7 +26,14 @@ public class User {
     private String accountId;
 
     @NotNull
-    @Column(length = 20)
+    @Column(length = 60)
     private String password;
 
+    @Builder
+    public User(int number, String name, String accountId, String password) {
+        this.number = number;
+        this.name = name;
+        this.accountId = accountId;
+        this.password = password;
+    }
 }
